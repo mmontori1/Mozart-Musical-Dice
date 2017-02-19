00fa483b9f2055cddf1ba907b3932266d9fb2a6c
Mariano <mmontori@umich.edu>
2017-02-19 17:05:45 -0500

    finished minuet generator
    -randomly generates an iteration of the minuets
    -combined .wav file outputted to file called dice.wav
    -plays combined song
    -cleaned up code/added comments

--- run.py
+++ run.py
@@ -3,58 +3,64 @@ import sys
 from random import randint
 
 def randomAudioFile():
-	count = 1
+	#array of filenames to return
 	infiles = []
+	count = 1
+
 	#create list of minuets
 	while(count < 17):
-		file = "M" + str(count)
+		file = "waves/M" + str(count)
 		rollOne = randint(1, 6)
 		rollTwo = randint(1, 6)
 		rollTotal = rollOne + rollTwo
 		file += "-" + str(rollTotal) + ".wav"
 		index = count - 1
+		print(file)
 		infiles.append(file)
 		count += 1
-	# for files in infiles:
-	# 	print files
+
+	return infiles
 
 def audioGenerator():
-	# f = wave.open("waves/M1.wav")
+	#files to read in and file to output to
+	infiles = randomAudioFile();
 	outfile = "dice.wav"
-	# infiles = randomAudioFile();
-	infiles = ["waves/M1-2.wav", "waves/M2-2.wav"]
 
+	#reads through each .wav file
+	#gets the parameters and frames of each .wav file
 	data = []
 	for infile in infiles:
 	    w = wave.open(infile, 'rb')
-	    data.append( [w.getparams(), w.readframes(w.getnframes())] )
+	    data.append([w.getparams(), w.readframes(w.getnframes())])
 	    w.close()
 
+	#generates the combined .wav file and returns it
 	output = wave.open(outfile, 'wb')
 	output.setparams(data[0][0])
-	output.writeframes(data[0][1])
-	output.writeframes(data[1][1])
+	count = 0
+	for val in data:
+		output.writeframes(val[1])
+		count += 1
 	output.close()
 	composition = wave.open("dice.wav");
 	return composition
 
 def main():
-	randomAudioFile()
-	# f = audioGenerator()
-	# p = pyaudio.PyAudio()
-	# chunk = 1024
-	# stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
-	# 			channels = f.getnchannels(),  
-	# 			rate = f.getframerate(),  
-	# 			output = True)  
-	# data = f.readframes(chunk)
-	# while data:  
-	# 	stream.write(data)  
-	# 	data = f.readframes(chunk)
+	f = audioGenerator()
+	p = pyaudio.PyAudio()
+	chunk = 1024
+	stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
+				channels = f.getnchannels(),  
+				rate = f.getframerate(),  
+				output = True)  
+	data = f.readframes(chunk)
+	while data:  
+		stream.write(data)  
+		data = f.readframes(chunk)
 
-	# stream.stop_stream()
-	# stream.close()
-	# p.terminate()
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
 
 if __name__ == "__main__":
 	main()
