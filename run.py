83fc624f84f14a5eb7782f4c947547c6a2ef5579
Mariano <mmontori@umich.edu>
2017-02-19 20:20:12 -0500

    finished trio generator
    -generates the trio
    -renamed trio .wav files
    -cleaned up code/comments
    -rearranged functions for ease of use

--- run.py
+++ run.py
@@ -2,65 +2,77 @@ import wave, pyaudio
 import sys
 from random import randint
 
-def randomAudioFile():
-	#array of filenames to return
+def randomAudioFiles():
+	# array of filenames to return
 	infiles = []
 	count = 1
 
-	#create list of minuets
+	# creates list of 16 randomly chosen measures for the minuet
 	while(count < 17):
 		file = "waves/M" + str(count)
 		rollOne = randint(1, 6)
 		rollTwo = randint(1, 6)
 		rollTotal = rollOne + rollTwo
 		file += "-" + str(rollTotal) + ".wav"
-		index = count - 1
-		print(file)
+		infiles.append(file)
+		count += 1
+
+	# creates list of 16 randomly chosen measures for the trio
+	while(count < 33):
+		file = "waves/T" + str(count)
+		roll = randint(1, 6)
+		file += "-" + str(roll) + ".wav"
 		infiles.append(file)
 		count += 1
 
 	return infiles
 
 def audioGenerator():
-	#files to read in and file to output to
-	infiles = randomAudioFile();
+	# files to read in and file to output to
+	infiles = randomAudioFiles();
 	outfile = "dice.wav"
 
-	#reads through each .wav file
-	#gets the parameters and frames of each .wav file
+	# reads through each .wav file
+	# gets the parameters and frames of each .wav file
 	data = []
 	for infile in infiles:
 	    w = wave.open(infile, 'rb')
 	    data.append([w.getparams(), w.readframes(w.getnframes())])
 	    w.close()
 
-	#generates the combined .wav file and returns it
+	# writes to the outfile 
+	# generates the combined .wav file and returns it
 	output = wave.open(outfile, 'wb')
 	output.setparams(data[0][0])
-	count = 0
 	for val in data:
 		output.writeframes(val[1])
-		count += 1
 	output.close()
 	composition = wave.open("dice.wav");
 	return composition
 
-def main():
-	f = audioGenerator()
+def playComposition():
+	# uses the generated file to play
+	mozart = audioGenerator()
 	p = pyaudio.PyAudio()
+
+	# plays through the entire song
 	chunk = 1024
-	stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
-				channels = f.getnchannels(),  
-				rate = f.getframerate(),  
+	stream = p.open(format = p.get_format_from_width(mozart.getsampwidth()),  
+				channels = mozart.getnchannels(),  
+				rate = mozart.getframerate(),  
 				output = True)  
-	data = f.readframes(chunk)
+	data = mozart.readframes(chunk)
 	while data:  
 		stream.write(data)  
-		data = f.readframes(chunk)
+		data = mozart.readframes(chunk)
 
+	# closes the stream once the songs over
 	stream.stop_stream()
 	stream.close()
 	p.terminate()
+
+def main():
+	playComposition()
 
 if __name__ == "__main__":
 	main()
